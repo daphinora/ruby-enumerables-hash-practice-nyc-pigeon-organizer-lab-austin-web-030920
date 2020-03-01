c53f8bdb3a635c4133d20910bacbf7435ab0e538
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 04:24:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,8 +14,10 @@ def nyc_pigeon_organizer(data)
         x.each { |i|
         if b_name === i
           list_of_pigeons[i][:color] << color.to_s
+        }
+      }
+    }
           #to_s turns the shoveled  integer into a string!!
-        
 end
 
 
