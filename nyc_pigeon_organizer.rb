dbe351693e0ff69d3f6346d2e4beccb676037f66
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 02:53:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   list_of_pigeons = {}
   
   data.each do {|key, value|
-    data.each do {|diffkey, diffval|
+    value.each do {|diffkey, diffval|
       
   }
 end
