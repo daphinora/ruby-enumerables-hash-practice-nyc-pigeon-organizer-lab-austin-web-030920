1aaf2ef282ab447588429ba69d5ddeac82fcc941
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 04:51:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,30 +1,17 @@
-# def nyc_pigeon_organizer(data)
-#   pigeons = {}
-  
-#   #att = "attribute" (so here attkey means like color/gender/lives and attvalue is the value)
-  
-#   data.each { |attkey, attvalue|
-#     attvalue.each { |call, names|
-#       names.each { |name|
-#         pigeons[name] ||= {}
-#         pigeons[name][attkey] ||= []
-#         pigeons[name][attkey].push(call.to_s)
-
-#   #to_s is basically turning the integers into strings!
-#       }}}
-# end 
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-
-  data.each do |attribute_name, attributes|
-    attributes.each do |attribute_value, pigeon_names|
-      pigeon_names.each do |name|
-        pigeon_list[name] ||= {}
-        pigeon_list[name][attribute_name] ||= []
-        pigeon_list[name][attribute_name].push(attribute_value.to_s)
+  pigeons = {}
+  
+  #att = "attribute" (so here attkey means like color/gender/lives and attvalue is the value)
+  
+  data.each do |attkey, attvalue|
+    attvalue.each do |call, names|
+      names.each do |name|
+        pigeons[name] ||= {}
+        pigeons[name][attkey] ||= []
+        pigeons[name][attkey].push(call.to_s)
       end
-    end 
+    end
   end
-
-  pigeon_list
 end
+
+  #to_s is basically turning the integers into strings!
