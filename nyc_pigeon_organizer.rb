2e8d2331cb799d2088079921be265c8dbd010199
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 04:31:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,5 @@
 def nyc_pigeon_organizer(data)
   list_of_pigeons = {}
   
-  data.each { |key, value|
-    value.each { |diffkey, diffvalue|
-      diffvalue.each { |names|
-      list_of_pigeons[names] = {
-        :color => [], :gender => [], :lives => []
-      }}}}
-      
-  x = list_of_pigeons.keys
-    data[:color].each { |color, name|
-      name.each { |b_name|
-        x.each { |i|
-        if b_name === i
-          list_of_pigeons[i][:color] << color.to_s
-      }}}
-          #to_s turns the shoveled  integer into a string!!
-end
 
 
