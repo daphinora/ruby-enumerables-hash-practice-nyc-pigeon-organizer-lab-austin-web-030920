700f70028c6da19d23f984d8226900146d32b1a3
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 04:44:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
   
-  data.each { |attribute_name, attribute|
-    attribute.each { |string, names|
+  data.each { |attribute_name, attributes|
+    attributes.each { |string, names|
       names.each { |name|
         pigeons[name] || = {}
         pigeons[color_gender_place] || = []
